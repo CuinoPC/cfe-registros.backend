aadf180f3d3a4b14acb6e3decf6b630b2f17f18b
CuinoPC <cuino.p@gmail.com>
2025-02-10 01:04:59 -0600

    Se implemento el crud para las terminales

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const terminalRoutes = require('./routes/terminalRoutes'); // Asegúrate de incluirlo aquí
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api', userRoutes);  // Agregar rutas de usuarios
+app.use('/api', userRoutes);
+app.use('/api', terminalRoutes); // Ahora carga las rutas de terminales también
 
 app.listen(5000, () => {
     console.log('Servidor corriendo en el puerto 5000');
