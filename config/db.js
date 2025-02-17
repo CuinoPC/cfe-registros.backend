af3f0f1326da950f8d4dd00c5ed9902f716ac265
CuinoPC <cuino.p@gmail.com>
2025-02-17 15:04:16 -0600

    Se cambiaron las credenciales a la bd en host

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: 'blzqfqsgkzgqustmlga6-mysql.services.clever-cloud.com',
+    user: 'uzhu0qv5zdij7fj8',
+    password: 'VmZMGm2YS6Hk37gKJbfZ',
+    database: 'blzqfqsgkzgqustmlga6'
 });
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        return;
+    }
     console.log('Conectado a la base de datos MySQL');
 });
 
