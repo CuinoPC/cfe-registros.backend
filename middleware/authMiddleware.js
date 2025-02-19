e3238ace21315dae25a7a23007f1231452a0b881
CuinoPC <cuino.p@gmail.com>
2025-02-19 13:19:36 -0600

    Se puso directo la clave para el auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+
+const SECRET_KEY = process.env.SECRET_KEY || 'Cuino2003'; // 🔹 Usa la variable de entorno si existe
 
 module.exports = (req, res, next) => {
     const token = req.header('Authorization');
     if (!token) return res.status(401).json({ error: 'Acceso denegado' });
 
     try {
-        const verified = jwt.verify(token, 'Cuino2003');
+        const verified = jwt.verify(token, SECRET_KEY);
         req.user = verified;
         next();
     } catch (err) {
